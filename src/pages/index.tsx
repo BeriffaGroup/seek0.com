c6e5d81a858e01cf6b1b4d8faecd14ae8210422e
Mathias Fiskbæk <fiskce@beriffa.com>
2025-02-24 08:56:11 +0100

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -205,7 +205,7 @@ export default function Home() {
       <div
           className={`mt-00 text-sm flex flex-row items-center font-medium text-muted-foreground select-none`}
         >
-          Seek0 by Beriffa Group © 2015-2024{" "}
+          Seek0 by Beriffa Group © 2015-2025{" "}
           
       <Link
             href={`https://www.beriffa.com`}
@@ -215,7 +215,7 @@ export default function Home() {
 
 
           </Link>
-          <Badge variant={`outline`}>Beriffa Cloud Version OS-4011-1280</Badge>
+          <Badge variant={`outline`}>Beriffa Cloud Version OS-5211-1280</Badge>
           </div>
           
     </main>
